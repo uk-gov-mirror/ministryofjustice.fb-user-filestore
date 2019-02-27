e3aa27ebd234090008abc2501cda1d77be02b042
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-02-27 06:11:32 +0000

    Return 400 error status if file size exceeds maximum

--- app/controllers/user_file_controller.rb
+++ app/controllers/user_file_controller.rb
@@ -1,8 +1,12 @@
 class UserFileController < ApplicationController
   def create
-    result = File.open("files/result", "w")
+    result = File.open('files/result', 'w')
     result.print(Base64.decode64(params[:file]))
     result.close
-    head :ok
+    file_size = File.size('files/result')
+
+    return head 400 if file_size > params[:policy][:max_size].to_i
+
+    head 200
   end
 end
