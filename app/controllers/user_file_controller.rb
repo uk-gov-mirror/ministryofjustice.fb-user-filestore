c2af8e00f7ece0a25989fa8e28cbfd6a8579cf0d
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-02-28 16:20:28 +0000

    Return error if mime type of file is not in the approved list

--- app/controllers/user_file_controller.rb
+++ app/controllers/user_file_controller.rb
@@ -1,11 +1,14 @@
 class UserFileController < ApplicationController
   def create
-    result = File.open('files/result', 'w')
-    result.print(Base64.decode64(params[:file]))
+    result = File.open('files/result', 'wb')
+    result.write(Base64.decode64(params[:file]))
     result.close
-    file_size = File.size('files/result')
 
+    file_size = File.size('files/result')
     return head 400 if file_size > params[:policy][:max_size].to_i
+
+    mime_type = `file --b --mime-type 'files/result'`.strip
+    return head 400 unless params[:policy][:allowed_types].include?(mime_type)
 
     head 200
   end
