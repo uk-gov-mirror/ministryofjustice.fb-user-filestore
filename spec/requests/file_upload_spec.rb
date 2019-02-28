59bd8c38ac295466e2bc8dc0a259e9569a2461f8
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-02-28 16:20:28 +0000

    Return error if mime type of file is not in the approved list

--- spec/requests/file_upload_spec.rb
+++ spec/requests/file_upload_spec.rb
@@ -39,18 +39,123 @@ describe 'FileUpload API', type: :request do
         end
       end
     end
+
+    describe 'check file types' do
+      context 'accepts supported mime types' do
+        describe 'plain text format' do
+          let(:file) { file_fixture('hello_world.txt').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+
+        describe 'word document format' do
+          let(:file) { file_fixture('random.docx').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+
+        describe 'excel spreadsheet format' do
+          let(:file) { file_fixture('spreadsheet.xlsx').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 400' do
+            expect(response).to have_http_status(400)
+          end
+        end
+
+        describe 'open document format' do
+          let(:file) { file_fixture('random.odt').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+
+        describe 'pdf format' do
+          let(:file) { file_fixture('document.pdf').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+
+        describe 'jpeg format' do
+          let(:file) { file_fixture('image.jpg').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+
+        describe 'png format' do
+          let(:file) { file_fixture('image.png').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 200' do
+            expect(response).to have_http_status(200)
+          end
+        end
+      end
+
+      context 'Mime types that are not supported' do
+        describe 'bmp format' do
+          let(:file) { file_fixture('bitmap.bmp').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 400' do
+            expect(response).to have_http_status(400)
+          end
+        end
+
+        describe 'html format' do
+          let(:file) { file_fixture('hello.html').read }
+          let(:encoded_file) { Base64.encode64(file) }
+          let(:json) { json_format(encoded_file) }
+
+          it 'has status 400' do
+            expect(response).to have_http_status(400)
+          end
+        end
+      end
+    end
   end
 
   def json_format(encoded_file)
     {
-        "iat": '{timestamp}',
-        "encrypted_user_id_and_token": '{userId+userToken encrypted via AES-256 with the serviceToken as the key}',
-        "file": encoded_file,
-        "policy": {
-            "allowed_types": [],
-            "max_size": '1024',
-            "expires": '28d'
-        }
+      "iat": '{timestamp}',
+      "encrypted_user_id_and_token": '{userId+userToken encrypted via AES-256 with the serviceToken as the key}',
+      "file": encoded_file,
+      "policy": {
+        "allowed_types": %w[
+          text/plain
+          application/vnd.openxmlformats-officedocument.wordprocessingml.document
+          application/msword
+          application/vnd.oasis.opendocument.text
+          application/pdf
+          image/jpeg
+          image/png
+          application/vnd.ms-excel
+        ],
+        "max_size": '10240',
+        "expires": '28d'
+      }
     }
   end
 end
