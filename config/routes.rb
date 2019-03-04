86c4fd4d8438e3d9e5efec2c723bc2cc7459bbf0
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-03-04 15:20:54 +0000

    Add get route and set x-header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/service/:service_slug/user/:user_id/:fingerprint', to: 'user_file#show'
   post '/service/:service_slug/user/:user_id', to: 'user_file#create'
 end
