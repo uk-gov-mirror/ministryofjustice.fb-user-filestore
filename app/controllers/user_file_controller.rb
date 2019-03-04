2912b80f719f52cd618c13a9383286f4e6da5a8b
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-03-04 14:35:56 +0000

    Refactor

--- app/controllers/user_file_controller.rb
+++ app/controllers/user_file_controller.rb
@@ -1,4 +1,7 @@
 class UserFileController < ApplicationController
+  SERVICE_TOKEN = 'some-service-token'.freeze
+  IV = '1234567890123451'.freeze
+
   def create
     result = File.open('files/result', 'wb')
     result.write(Base64.decode64(params[:file]))
@@ -10,8 +13,14 @@ class UserFileController < ApplicationController
     mime_type = `file --b --mime-type 'files/result'`.strip
     return error_unsupported_file_type(mime_type) unless params[:policy][:allowed_types].include?(mime_type)
 
-    render json: { name: 'success' }, status: 200
+    filename = generate_filename(result, params[:user_id], SERVICE_TOKEN)
+    encrypted_filename = encrypt_filename(params[:encrypted_user_id_and_token], filename, IV)
+    hashed_filename = hashed_digest(encrypted_filename)
+
+    render json: { name: full_filename(params[:service_slug], params[:user_id], hashed_filename) }, status: 200
   end
+
+  private
 
   def error_large_file(size)
     render json: { code: 400,
@@ -26,12 +35,12 @@ class UserFileController < ApplicationController
                    type: type }, status: 400
   end
 
-  def self.generate_filename(file, user_id, service_token)
+  def generate_filename(file, user_id, service_token)
     checksum = Digest::SHA1.file(file).to_s
     Digest::SHA1.hexdigest(service_token + user_id + checksum)
   end
 
-  def self.encrypt_filename(key, filename, iv)
+  def encrypt_filename(key, filename, iv)
     cipher = OpenSSL::Cipher.new 'AES-256-CBC'
     cipher.encrypt
     cipher.iv = iv
@@ -40,7 +49,11 @@ class UserFileController < ApplicationController
     encrypted.unpack1('H*')
   end
 
-  def self.hashed_digest(filename)
+  def hashed_digest(filename)
     Digest::SHA1.hexdigest(filename)
+  end
+
+  def full_filename(service_slug, user_id, filename)
+    "#{service_slug}/#{user_id}/#{filename}"
   end
 end
