d54166a1d5a38eb9a4f17fbf8f3b95913b5d614f
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-03-11 09:52:52 +0000

    enforce request must be application/json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include Concerns::ErrorHandling
   include Concerns::JWTAuthentication
+
+  before_action :enforce_json_only
+
+  private
+
+  def enforce_json_only
+    response.status = :unacceptable unless request.format.json?
+  end
 end
