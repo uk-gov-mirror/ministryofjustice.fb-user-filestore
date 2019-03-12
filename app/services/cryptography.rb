6fec127dc077dc10a90f2b032ed71ea579902b82
Andrien Ricketts <andrien.ricketts@digital.justice.gov.uk>
2019-03-12 12:25:49 +0000

    ENV variables to be used to encrypt/decrypt files

--- app/services/cryptography.rb
+++ app/services/cryptography.rb
@@ -1,29 +1,33 @@
 class Cryptography
-
-  IV = '1234567890123451'.freeze
-  KEY = 'a9c12000de36def805e4c2107bd8e910'.freeze
-
   def initialize(file:)
     @file = file
   end
 
   def encrypt
     cipher.encrypt
-    cipher.iv = IV
-    cipher.key = KEY
+    cipher.iv = encryption_iv
+    cipher.key = encryption_key
     encrypted_data = cipher.update(@file) + cipher.final
     encrypted_data.unpack1('H*')
   end
 
   def decrypt
     cipher.decrypt
-    cipher.iv = IV
-    cipher.key = KEY
+    cipher.iv = encryption_iv
+    cipher.key = encryption_key
     data = [@file].pack('H*').unpack('C*').pack('c*')
     cipher.update(data) + cipher.final
   end
 
   def cipher
     @cipher ||= OpenSSL::Cipher.new 'AES-256-CBC'
+  end
+
+  def encryption_iv
+    ENV['ENCRYPTION_IV']
+  end
+
+  def encryption_key
+    ENV['ENCRYPTION_KEY']
   end
 end
