8c9d192434dd4432b3978387f07b56765b33f63b
Phil Lee <phil.lee@digital.justice.gov.uk>
2019-03-26 16:25:57 +0000

    comment bucket lifecycle configuration for now

--- config/initializers/configure_bucket_lifecycle.rb
+++ config/initializers/configure_bucket_lifecycle.rb
@@ -1,21 +1,23 @@
-if ENV['AWS_ACCESS_KEY_ID'] &&
-   ENV['AWS_SECRET_ACCESS_KEY'] &&
-   ENV['AWS_REGION'] &&
-   ENV['AWS_S3_BUCKET_NAME']
-  client = Aws::S3::Client.new
-
-  client.put_bucket_lifecycle_configuration({
-    bucket: ENV['AWS_S3_BUCKET_NAME'],
-    lifecycle_configuration: {
-      rules: [{
-        expiration: { days: 29 },
-        id: '28d',
-        filter: { prefix: '28d/' },
-        status: 'Enabled',
-        abort_incomplete_multipart_upload: {
-          days_after_initiation: 29 }
-        }
-      ]
-    }
-  })
-end
+# Not possible until correct permission applied to credentials
+#
+# if ENV['AWS_ACCESS_KEY_ID'] &&
+#    ENV['AWS_SECRET_ACCESS_KEY'] &&
+#    ENV['AWS_REGION'] &&
+#    ENV['AWS_S3_BUCKET_NAME']
+#   client = Aws::S3::Client.new
+#
+#   client.put_bucket_lifecycle_configuration({
+#     bucket: ENV['AWS_S3_BUCKET_NAME'],
+#     lifecycle_configuration: {
+#       rules: [{
+#         expiration: { days: 29 },
+#         id: '28d',
+#         filter: { prefix: '28d/' },
+#         status: 'Enabled',
+#         abort_incomplete_multipart_upload: {
+#           days_after_initiation: 29 }
+#         }
+#       ]
+#     }
+#   })
+# end
